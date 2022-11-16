561a0903510a114ad91ab7e3a8bf4387dd6f7223
oscar08850 <oscar08850@gmail.com>
2022-11-16 12:05:18 +0100

    Estructura basica

---
+++ EJ25-3/src/main/java/com/example/EJ25/Ej253Application.java
@@ -0,0 +1,13 @@
+package com.example.EJ25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ej253Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ej253Application.class, args);
+	}
+
+}
