a23d7232cc4bc9b6428e31a39d83592a51367daa
oscar08850 <oscar08850@gmail.com>
2022-11-16 12:29:14 +0100

    DAO

---
+++ EJ25-3/src/main/java/com/example/EJ25/dao/ICajasDAO.java
@@ -0,0 +1,9 @@
+package com.example.EJ25.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.EJ25.dto.Caja;
+
+public interface ICajasDAO extends JpaRepository<Caja, Long> {
+
+}
